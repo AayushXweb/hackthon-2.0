c1c377acda2a81c3266cc5cb2ff5b44321ca68b2
princeXweb <princexbundela@gmail.com>
2025-08-24 21:34:42 +0530

    first task

---
+++ pricnePq/pq1/script.js
@@ -0,0 +1,21 @@
+//Day 1 Print student details from an object
+class Student {
+  constructor(name, Sem, Enrol) {
+    this.name = name;
+    this.Sem = Sem;
+    this.Enrol = Enrol;
+  }
+
+  printDetails() {
+    console.log(`Name: ${this.name}`);
+    console.log(`Sem: ${this.Sem}`);
+    console.log(`Enrol: ${this.Enrol}`);
+  }
+}
+
+const student1 = new Student(
+  prompt("Enter name:"),
+  prompt("Enter Sem:"),
+  prompt("Enter Enrol:")
+);
+student1.printDetails();
