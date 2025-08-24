d300c728ec6c336d3acf395b2998a7a35891021e
Radiator-Kratos <mdkamilkausharmallick@gmail.com>
2025-08-24 23:47:41 +0530

    Add files via upload

---
+++ day1.js
@@ -0,0 +1,14 @@
+// Create an object
+let student = {
+    name: "Md Kamil Kaushar Mallick",
+    age: 20,
+    course: "Computer Science",
+    rollNo: 36
+};
+
+// Print details
+console.log("Student Details:");
+console.log("Name: " + student.name);
+console.log("Age: " + student.age);
+console.log("Course: " + student.course);
+console.log("Roll No: " + student.rollNo);
