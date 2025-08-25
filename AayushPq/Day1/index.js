1e74c494e06c09f41c5291769cfe8b12538f72fd
AayushXweb <kumaraayush937@gmail.com>
2025-08-25 14:33:09 +0530

    day1
    #day1

---
+++ AayushPq/Day1/index.js
@@ -0,0 +1,11 @@
+let student = {
+  name: "Aayush kumar",
+  course: "b.tech",
+  passingYear: 2024,
+};
+
+// Print details
+console.log("Student Details:");
+console.log("Name: " + student.name);
+console.log("Course: " + student.course);
+console.log("Roll No: " + student.passingYear);
